594f0a895bd39002ddd48ed31c5c7668fcf3a53e
ksenyavol <120124961+ksenyavol@users.noreply.github.com>
2023-04-20 20:46:11 +0300

    Update work10_5.py

--- DZ10/work10_5.py
+++ DZ10/work10_5.py
@@ -1,20 +1,21 @@
-# 5.Выполнить пинг веб-ресурсов yandex.ru, youtube.com и
-# преобразовать результаты из байтовового в строковый тип на кириллице.
-
-import subprocess
-import chardet
-import os
-
-ARGS = ['ping', 'yandex.ru']
-YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-for line in YA_PING.stdout:
-    res = chardet.detect(line)
-    print(line.decode(encoding=res['encoding']))
-
-ARGS = ['ping', 'youtube.com']
-YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-for line in YA_PING.stdout:
-    res = chardet.detect(line)
-    print(line.decode(encoding=res['encoding']))
-
-print(os.name)
+ # 5.Выполнить пинг веб-ресурсов yandex.ru, youtube.com и
+ # преобразовать результаты из байтовового в строковый тип на кириллице.
+ 
+ import subprocess
+ import chardet
+ import os
+ 
+ ARGS = ['ping', 'yandex.ru']
+ YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+ for line in YA_PING.stdout:
+     res = chardet.detect(line)
+     print(line.decode(encoding=res['encoding']))
+ 
+ ARGS = ['ping', 'youtube.com']
+ YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+ for line in YA_PING.stdout:
+     res = chardet.detect(line)
+     print(line.decode(encoding=res['encoding']))
+ 
+ print(os.name)
+ 
